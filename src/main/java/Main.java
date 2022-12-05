2999534db2a7d0d91e52570cb7d1b054c3aa00d2
Shai Givati <shai@gmail.com>
2022-12-05 09:39:08 +0200

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        AxisSystem axisSystem = new AxisSystem();
+        axisSystem.addSinglePoint(200, 200, "red");
+        axisSystem.addSinglePoint(100, 200, "blue");
+    }
+
+}
